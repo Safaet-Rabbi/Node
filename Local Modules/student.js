ef237f969d1f1669fa2267244405cbab6ec73165
Safaet-Rabbi <86919997+Safaet-Rabbi@users.noreply.github.com>
2024-06-11 22:16:02 +0600

    Add files via upload

---
+++ Local Modules/student.js
@@ -0,0 +1,19 @@
+const getName = () =>{
+    return "Safaet Rabbi";
+}
+
+const getAge = () => {
+    return "22";
+}
+
+const cgpa = 3.92;
+
+// exports.getName = getName;
+// exports.getAge = getAge;
+// exports.result = cgpa;
+
+module.exports = {
+    getName,
+    getAge,
+    cgpa
+}
